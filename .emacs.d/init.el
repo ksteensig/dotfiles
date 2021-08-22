57c82bae6cc78300abc22d153e691cc1c747304b
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2021-08-22 17:01:23 +0200

    Synchronize actual emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+(setq inhibit-startup-screen t)
+
+(load-theme 'light-blue)
+
+(setq ansi-color-faces-vector
+      [default default default italic underline success warning error])
+
+(global-display-line-numbers-mode t)
+
+(require 'jupyter)
