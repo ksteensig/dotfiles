f24c16d9271387fe8fe71c386bd9e8a9aef707c1
Kasper Steensig <kasper.steensig@gmail.com>
2021-08-23 23:15:19 +0200

    Add org-roam and magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,4 +7,43 @@
 
 (global-display-line-numbers-mode t)
 
-(require 'jupyter)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit use-package org-roam htmlize)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq org-directory
+      "~/Workspace/notes/org")
+
+(use-package org-roam
+    :custom
+    (org-roam-directory org-directory) 
+    :bind (("C-c n l" . org-roam-buffer-toggle)
+           ("C-c n f" . org-roam-node-find)
+           ("C-c n i" . org-roam-node-insert)
+	   :map org-mode-map
+           ("C-M-i"    . completion-at-point))
+    ; global-page-break-lines-mode will break the org-roam buffer
+    :config
+    (org-roam-setup))
+
+(setq org-roam-v2-ack t)
+
+(setq org-cite-activate-processor 'basic)
+
+(use-package magit)
