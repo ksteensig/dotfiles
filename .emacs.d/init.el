4c757ff2e8c0b7a9992fbe6bcfa444950d6cdf70
Kasper Steensig <kasper.steensig@gmail.com>
2021-08-24 20:36:23 +0200

    Add new packages and set up org-dailies:
    New packages:
    
      * zenburn theme
      * org-bullets
      * vertico
      * which-key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,5 @@
 (setq inhibit-startup-screen t)
 
-(load-theme 'light-blue)
-
 (setq ansi-color-faces-vector
       [default default default italic underline success warning error])
 
@@ -11,15 +9,17 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit use-package org-roam htmlize)))
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" default))
+ '(package-selected-packages
+   '(which-key marginalia vertico org-bullets zenburn-theme magit use-package org-roam htmlize)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -27,23 +27,80 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; org-mode
+
 (setq org-directory
       "~/Workspace/notes/org")
 
 (use-package org-roam
-    :custom
-    (org-roam-directory org-directory) 
-    :bind (("C-c n l" . org-roam-buffer-toggle)
-           ("C-c n f" . org-roam-node-find)
-           ("C-c n i" . org-roam-node-insert)
-	   :map org-mode-map
-           ("C-M-i"    . completion-at-point))
-    ; global-page-break-lines-mode will break the org-roam buffer
-    :config
-    (org-roam-setup))
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory org-directory)
+  (org-roam-completion-everywhere t)
+  (org-roam-dailies-capture-templates
+    '(("d" "default" entry "* %<%I:%M %p>: %?"
+       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode))
 
 (setq org-roam-v2-ack t)
 
-(setq org-cite-activate-processor 'basic)
+(setq org-hide-emphasis-markers t)
+
+(use-package org-bullets
+    :config
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; magit
 
 (use-package magit)
+
+;; vertico
+
+(use-package vertico
+  :ensure t
+  :bind (:map vertico-map
+         ("C-j" . vertico-next)
+         ("C-k" . vertico-previous)
+         ("C-f" . vertico-exit)
+         :map minibuffer-local-map
+         ("M-h" . backward-kill-word))
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+;; savehist
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; marginalia
+
+(use-package marginalia
+  :after vertico
+  :ensure t
+  :custom
+  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+  :init
+  (marginalia-mode))
+
+
+;; which-key
+
+(use-package which-key)
+(which-key-mode)
